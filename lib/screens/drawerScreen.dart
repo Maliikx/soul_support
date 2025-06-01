78a01fb4387d648825a5c7ce29566b199c001525
Samamohamed11 <145224947+Samamohamed11@users.noreply.github.com>
2025-06-01 15:23:04 +0300

    Update drawerScreen.dart

--- lib/screens/drawerScreen.dart
+++ lib/screens/drawerScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:hive/hive.dart';
 import 'package:soul_support/main.dart';
 import 'package:soul_support/screens/homeScreen.dart';
 import 'package:soul_support/screens/Setting%20and%20proffile%20screens/profile_screen.dart';
@@ -8,182 +9,224 @@ import 'package:soul_support/screens/Setting%20and%20proffile%20screens/setting_
 import 'package:soul_support/transitions/custom_transitions.dart';
 import 'package:soul_support/widgets/screenWrapper.dart';
 
+
+
 class DrawerScreen extends StatefulWidget {
+
   const DrawerScreen({super.key});
 
   @override
   State<DrawerScreen> createState() => _DrawerScreenState();
 }
 
+
 class _DrawerScreenState extends State<DrawerScreen> {
+
+  String? username;
+
+  @override
+  void initState() {
+    super.initState();
+    loadUsername();
+  }
+
+  void loadUsername() async {
+    var box = Hive.box('myBox');
+    String storedUsername = box.get('username', defaultValue: 'Guest');
+    setState(() {
+      username = storedUsername;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: primary,
       body: Stack(
-        children: [
-          Positioned(
-              top: 17,
-              left: -214,
-              child: Transform.rotate(
-                angle: 0.12,
-                child: Container(
-                  width: 720,
-                  height: 750,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [const Color.fromARGB(255, 0, 140, 195), const Color.fromARGB(0, 151, 202, 219)],
-                         begin: Alignment.topCenter,
-                         end: Alignment.bottomCenter,
-                         stops: [0.0, 1],
-                         ),
-                          shape: BoxShape.circle
-                              ),
+          children: [
+            Positioned(
+                top: 17,
+                left: -214,
+                child: Transform.rotate(
+                  angle: 0.12,
+                  child: Container(
+                    width: 720,
+                    height: 750,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                          const Color.fromARGB(255, 0, 140, 195),
+                          const Color.fromARGB(0, 151, 202, 219)
+                        ],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          stops: [0.0, 1],
                         ),
-              )
-              ),
-              Positioned(
-              top: 645,
-              left: 190,
-              child: Transform.rotate(
-                angle: 0.12,
-                child: Container(
-                  width: 340,
-                  height: 340,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [const Color.fromARGB(255, 244, 223, 218), const Color.fromARGB(0, 244, 223, 218)],
-                         begin: Alignment.topCenter,
-                         end: Alignment.bottomCenter,
-                         stops: [0, 1],
-                         ),
-                          shape: BoxShape.circle
-                              ),
-                        ),
-              )
-              ),
-          Positioned(
-            left: 25,
-            top: 65,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      width: 50,
-                      height: 50,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: Colors.white,
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(50),
-                        child: Image.asset("assets/imgs/profilePicture.png",
-                        fit: BoxFit.cover,
-                        alignment: Alignment.topCenter
-                        ),
-                      ),
+                        shape: BoxShape.circle
                     ),
-                    SizedBox(width: 10,),
-                    Text("Hello Ahmed", style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                     color: Colors.white),)
-                  ],
-                ),
-                SizedBox(
-                  height: 60,
-                ),
-
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushReplacement(context, 
-                    fadeTransition(ScreenWrapper(child: const HomeScreen())));
-                  },
-                  child: Row(
-                    children: [
-                      SvgPicture.asset('assets/svg/homeIcon.svg',
-                      width: 42,
-                      color: Colors.white,
-                      
-                      ),
-                      SizedBox(width: 20,),
-                      Text("Home", style: 
-                      TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                       color: Colors.white),),
-                    ],
                   ),
-                ),
-                SizedBox(height: 25,),
-                GestureDetector(
-                  onTap: () {
-                     Navigator.pushReplacement(context, 
-                    slideLtR(ScreenWrapper(child: const ProgramScreen())));
-                  
-                  },
-                  child: Row(
-                    children: [
-                      SvgPicture.asset('assets/svg/programIcon.svg',
-                      width: 42,
-                      color: Colors.white,
-                      
-                      ),
-                      SizedBox(width: 20,),
-                      Text("My Program", style: 
-                      TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                       color: Colors.white),),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 25,),
-                GestureDetector(
-                  onTap: () {
-                     Navigator.push(context, 
-                    slideLtR(ScreenWrapper(child: const setting_screen())));
-                  },
-                  child: Row(
-                    children: [
-                      SvgPicture.asset('assets/svg/settingsIcon.svg',
-                      width: 42,
-                      color: Colors.white,
-                      
-                      ),
-                      SizedBox(width: 20,),
-                      Text("Settings", style: 
-                      TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                       color: Colors.white),),
-                    ],
-                  ),
-                ),
-                
-              ],
+                )
             ),
-          ),
-          Positioned(
-            left: 25,
-            bottom: 80,
-            child: Row(
-              children: [
-                SvgPicture.asset('assets/svg/logoutIcon.svg',
-                    width: 35,
-                    color: Colors.white,
-                    
+            Positioned(
+                top: 645,
+                left: 190,
+                child: Transform.rotate(
+                  angle: 0.12,
+                  child: Container(
+                    width: 340,
+                    height: 340,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                          const Color.fromARGB(255, 244, 223, 218),
+                          const Color.fromARGB(0, 244, 223, 218)
+                        ],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          stops: [0, 1],
+                        ),
+                        shape: BoxShape.circle
+                    ),
+                  ),
+                )
+            ),
+            Positioned(
+              left: 25,
+              top: 65,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.white,
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(50),
+                          child: Image.asset("assets/imgs/logo_girl.png",
+                              fit: BoxFit.cover,
+                              alignment: Alignment.topCenter
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 10,),
+                      Text("Hello $username ", style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),)
+                    ],
+                  ),
+                  SizedBox(
+                    height: 60,
+                  ),
+
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacement(context,
+                          fadeTransition(
+                              ScreenWrapper(child: const HomeScreen())));
+                    },
+                    child: Row(
+                      children: [
+                        SvgPicture.asset('assets/svg/homeIcon.svg',
+                          width: 42,
+                          color: Colors.white,
+
+                        ),
+                        SizedBox(width: 20,),
+                        Text("Home", style:
+                        TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 25,),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacement(context,
+                          slideLtR(ScreenWrapper(child: const ProgramScreen())));
+                    },
+                    child: Row(
+                      children: [
+                        SvgPicture.asset('assets/svg/programIcon.svg',
+                          width: 42,
+                          color: Colors.white,
+
+                        ),
+                        SizedBox(width: 20,),
+                        Text("My Program", style:
+                        TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 25,),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          slideLtR(ScreenWrapper(child: const setting_screen())));
+                    },
+                    child: Row(
+                      children: [
+                        SvgPicture.asset('assets/svg/settingsIcon.svg',
+                          width: 42,
+                          color: Colors.white,
+
+                        ),
+                        SizedBox(width: 20,),
+                        Text("Settings", style:
+                        TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),),
+                      ],
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+            Positioned(
+              left: 25,
+              bottom: 80,
+              child: GestureDetector(
+                onTap: () async {
+                  // Clear the username from Hive
+                  var box = Hive.box('myBox');
+                  await box.delete('username');
+
+                  // Optionally clear any other data you want here
+
+                  // Navigate to the login screen (change route name as needed)
+                  Navigator.pushReplacementNamed(context, '/start');
+                },
+                child: Row(
+                  children: [
+                    SvgPicture.asset('assets/svg/logoutIcon.svg',
+                      width: 35,
+                      color: Colors.white,
                     ),
                     SizedBox(width: 5,),
-                Text("Log out",
-                style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                     color: Colors.white),)
-              ],
-            ))
-        ],
-      ),
+                    Text("Log out",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ]),
     );
   }
+
 }
