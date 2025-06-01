37a2ec73094875b7ed64f224c163d13d9c69fc9a
Maliikx <eljuoker@gmail.com>
2025-06-01 12:29:12 +0300

    created type adapter for note using hive

--- lib/data/note_model.dart
+++ lib/data/note_model.dart
@@ -1,6 +1,14 @@
-class NoteModel {
+import 'package:hive_flutter/hive_flutter.dart';
+
+
+part 'note_model.g.dart';
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String content;
+  @HiveField(2)
   final DateTime date;
 
   NoteModel({
